5a8b3a9afbb905acbc79ac4aef8426593c55d275
Hieu Le <hieu.le@student.ufv.ca>
2020-06-20 15:13:52 -0700

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@f23=5qcn^(40&secyk!)3hxe=ucm&hau35^zu&4k5+^g0_15c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ufvhub.herokuapp.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Media folder
